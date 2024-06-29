30dfaecc5ec972c512c17546b2aa5f1bad586a15
Rishabh Kumar <rishabh.6542@gmail.com>
2024-06-29 17:23:37 +0530

    revert

--- db/index.ts
+++ db/index.ts
@@ -2,8 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as dbSchema from "@/schemas/dbSchema";
 
-const connectionString =
-  "postgresql://ai-chatbot_owner:dKpThP6UG0Hv@ep-wispy-firefly-a1279od3.ap-southeast-1.aws.neon.tech/ai-chatbot?sslmode=require";
+const connectionString = process.env.AUTH_DRIZZLE_URL || "";
 const pool = neon(connectionString);
 
 export const db = drizzle(pool, {
