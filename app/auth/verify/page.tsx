7d612cab87f31327aa3abf6c08fab8cbde210645
Rishabh Kumar <rishabh.6542@gmail.com>
2024-07-04 21:37:07 +0530

    minor fix

--- app/auth/verify/page.tsx
+++ app/auth/verify/page.tsx
@@ -8,21 +8,42 @@ import { useCallback, useEffect, useState } from "react";
 
 function Verify() {
   const params = useSearchParams();
+  const [isLoading, setIsLoading] = useState(true); // Initial loading state
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
   const token = params.get("token");
-  const verifyUser = useCallback(() => {
+
+  const verifyUser = useCallback(async () => {
     if (!token) return;
-    verify(token).then((data) => {
+
+    setIsLoading(true); // Show loading indicator before verification
+
+    try {
+      const data = await verify(token);
       setError(data?.error);
       setSuccess(data?.success);
-    });
+    } catch (error) {
+      console.error("Error verifying user:", error);
+      setError("An error occurred during verification. Please try again.");
+    } finally {
+      setIsLoading(false); // Hide loading indicator after verification
+    }
   }, [token]);
+
   useEffect(() => {
     verifyUser();
   }, [verifyUser]);
+
   return (
     <div>
+      {isLoading && (
+        <div className="flex space-x-2 justify-center items-center bg-white h-screen dark:invert">
+          <span className="sr-only">Loading...</span>
+          <div className="h-8 w-8 bg-black rounded-full animate-bounce animation-delay:-0.3s"></div>
+          <div className="h-8 w-8 bg-black rounded-full animate-bounce animation-delay:-0.15s"></div>
+          <div className="h-8 w-8 bg-black rounded-full animate-bounce"></div>
+        </div>
+      )}
       <FormSuccess message={success} />
       <FormError message={error} />
     </div>
